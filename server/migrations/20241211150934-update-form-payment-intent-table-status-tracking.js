ed493eb291b47635cf7a0dfe1199c80e4438e8d3
Saul Reyes <reyessaul796@gmail.com>
2025-01-16 15:11:20 -0800

    feat: migration to add status indicator columns FormPaymentIntents tables

---
+++ server/migrations/20241211150934-update-form-payment-intent-table-status-tracking.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    try {
+      await queryInterface.addColumn("FormPaymentIntents", "stripe_pi_status", {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+        transaction,
+      });
+
+      await queryInterface.addColumn("FormPaymentIntents", "expires_at", {
+        // set it to null when payment.intent has not been completed
+        type: Sequelize.DATE,
+        allowNull: true,
+        transaction,
+      });
+
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("FormPaymentIntents", "stripe_pi_status");
+    await queryInterface.removeColumn("FormPaymentIntents", "expires_at");
+  },
+};
