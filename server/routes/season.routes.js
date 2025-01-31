ad18ceaaa1678a2de62c9e7a29a8964856fc602c
Armando Banuelos <armando.banuelos@unnaturalproducts.com>
2025-01-30 22:32:48 -0800

    feat: adding seasons APIs to swagger

--- server/routes/season.routes.js
+++ server/routes/season.routes.js
@@ -9,13 +9,13 @@ const router = Router();
 
 module.exports = (checkJwt) => {
   router.get("/", SeasonController.getAllSeasons);
-  router.get("/:id", SeasonController.getSeason);
-  router.get("/:id/leagues", SeasonController.getSeasonsByLeagueId);
-  router.get("/:id/divisions", DivisionController.getBySeasonId);
-  router.get("/:seasonId/seasonId", DivisionController.getAllDivsionsBySeason);
+  router.get("/:season_id", SeasonController.getSeason);
+  router.get("/leagues/:league_id", SeasonController.getSeasonsByLeagueId);
+  router.get("/divisions/:division_id", DivisionController.getBySeasonId);
+  router.get("/seasons/:season_id", DivisionController.getAllDivsionsBySeason);
   router.post("/", SeasonController.createSeason);
-  router.patch("/:id", SeasonController.updateSeason);
-  router.delete("/:id", SeasonController.deleteSeason);
+  router.put("/:season_id", SeasonController.updateSeason);
+  router.delete("/:season_id", SeasonController.deleteSeason);
 
   return router;
 };
